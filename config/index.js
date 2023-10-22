4c5486f11f67b80ba4bc1f4a7589f3cb20ca0061
Alfredo <21030761itcelaya.edu.mx>
2023-10-22 15:32:08 -0600

    Mysql dependencie to persist data. Using free mysql server host

--- config/index.js
+++ config/index.js
@@ -5,4 +5,10 @@ module.exports = {
     jwt:{
         secret: process.env.JWT_SECRET || 'notasecret',
     },
+    mysql:{
+        host: process.env.MYSQL_HOST || 'sql10.freemysqlhosting.net',
+        user: process.env.MYSQL_USER || 'sql10655544',
+        password: process.env.MYSQL_PASS|| '8rtc3ibmpr',
+        database: process.env.MYSQL_DB || 'sql10655544',
+    }
 }
