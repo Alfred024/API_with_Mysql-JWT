77f0e73ee4d87516000b539596c0a056ae65f9f2
Alfredo <21030761@itcelaya.edu.mx>
2023-11-06 00:08:06 -0600

    Change DB vars

--- config/index.js
+++ config/index.js
@@ -6,9 +6,9 @@ module.exports = {
         secret: process.env.JWT_SECRET || 'notasecret',
     },
     mysql:{
-        host: process.env.MYSQL_HOST || 'sql10.freemysqlhosting.net',
-        user: process.env.MYSQL_USER || 'sql10655544',
-        password: process.env.MYSQL_PASS|| '8rtc3ibmpr',
-        database: process.env.MYSQL_DB || 'sql10655544',
+        host: process.env.MYSQL_HOST || 'sql9.freemysqlhosting.net',
+        user: process.env.MYSQL_USER || 'sql9659595',
+        password: process.env.MYSQL_PASS|| 'Ke1mAqZs1t',
+        database: process.env.MYSQL_DB || 'sql9659595',
     }
 }
