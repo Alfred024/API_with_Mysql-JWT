61ba9c3829e83f41df88b58a22fbe24c66e3f16f
Alfredo <21030761@itcelaya.edu.mx>
2023-11-09 22:57:09 -0600

    Creation of controller functions and secure post file to check auth

--- api/components/post/controller.js
+++ api/components/post/controller.js
@@ -1,16 +1,63 @@
-const TABLA = 'post';
+const TABLE = 'POST';
 
 module.exports = function (injectedStore) {
     let store = injectedStore;
-    if (!store) {
-        store = require('../../../store/mysql');
-    }
+	if (!store) {
+		store = require('../../../store//mysql');
+	}
 
-    function list() {
-        return store.list(TABLA);
-    }
+	function list(query) {
+		return store.list(TABLE);
+	}
 
-    return {
-        list,
-    };
+	async function get(id) {
+		const user = await store.get(TABLE, id);
+		if (!user) {
+			throw error('No existe el post', 404);
+		}
+
+		return user;
+	}
+
+	async function upsert(data, user) {
+		const post = {
+			id: data.id,
+			user: user,
+			text: data.text,
+		}
+
+		if (!post.id) {
+			post.id = nanoid();
+		}
+
+		return store.upsert(TABLE, post).then(() => post);
+	}
+
+	async function like(post, user) {
+        const like = await store.upsert(TABLE + '_like', {
+            post: post,
+            user: user,
+        });
+
+        return like;
+	}
+
+	async function postsLiked(user) {
+		const users = await store.query(TABLE + '_like', { user: user }, {post: post});
+		return users;
+	}
+
+	async function postLikers(post) {
+		const users = await store.query(TABLE + '_like', { post: post }, {post: post});
+		return users;
+	}
+
+	return {
+		list,
+		get,
+		upsert,
+		like,
+		postsLiked,
+		postLikers,
+	}
 }
--- api/components/post/network.js
+++ api/components/post/network.js
@@ -2,17 +2,65 @@ const express = require('express');
 
 const response = require('../../../network/response');
 const Controller = require('./index');
+const auth = require('./secure');
 
 const router = express.Router();
 
-router.get('/', list);
-
-function list(req, res, next) {
+router.get('/', auth('list'),(req, res, next)=> {
     Controller.list()
-        .then(data => {
-            response.success(req, res, data, 200);
+		.then(post => {
+			response.succes(req, res, post, 200);
+		})
+		.catch(next);
+});
+
+router.get('/:id', auth('get'), (req, res, next) => {
+	Controller.get(req.params.id)
+		.then(post => {
+			response.succes(req, res, post, 200);
+		})
+		.catch(next);
+})
+
+router.get('/like', auth('list_own'), (req, res, next) => {
+    Controller.postsLiked(req.user.sub)
+		.then(post => {
+			response.succes(req, res, post, 200);
+		})
+		.catch(next);
+});
+    
+router.get('/:id/like', auth('list'), (req, res, next) =>{
+    Controller.postLikers(req.params.id)
+        .then(post => {
+            response.succes(req, res, post, 200);
         })
         .catch(next);
-}
+});
+
+router.post('/', auth('add') ,(req, res, next)=> {
+	Controller.upsert(req.body, req.user.id)
+		.then(post => {
+			response.succes(req, res, post, 201);
+		})
+		.catch(next);
+});
+
+router.put('/', auth('update', { owner: 'user' }), (req, res, next)=> {
+	Controller.upsert(req.body, req.user.id)
+		.then(post => {
+			response.succes(req, res, post, 201);
+		})
+		.catch(next);
+});
+
+router.post('/:id/like', auth('add'),(req, res, next) =>{
+	Controller.like(req.params.id, req.user.sub)
+		.then(post => {
+			response.succes(req, res, post, 201);
+		})
+		.catch(next);
+})
+
 
 module.exports = router;
